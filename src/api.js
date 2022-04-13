bbbc98e63a8deca875afbf41b90d8182df9c4059
chiggy69 <chiragaggarwal69@gmail.com>
2022-04-14 02:14:10 +0530

    api functionality added

---
+++ src/api.js
@@ -0,0 +1,36 @@
+//Base url
+const API_KEY = process.env.REACT_APP_API_KEY;
+const base_url = `https://api.rawg.io/api/games?key=${API_KEY}`;
+
+//Getting current date
+const getCurrentMonth = () => {
+	const month = new Date().getMonth() + 1;
+	if (month < 10) {
+		return `0${month}`;
+	} else {
+		return month;
+	}
+};
+
+const getCurrentDay = () => {
+	const day = new Date().getDate();
+	if (day < 10) {
+		return `0${day}`;
+	} else {
+		return day;
+	}
+};
+
+//current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//popular games
+const popular_games = `&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
