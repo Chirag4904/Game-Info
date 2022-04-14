d6a8256bc581b180ebfb3bc35c6434789eea0d44
chiggy69 <chiragaggarwal69@gmail.com>
2022-04-14 15:31:16 +0530

    typo fixed

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -2,9 +2,9 @@ import axios from "axios";
 import { popularGamesUrl } from "../api";
 
 //Action creator
-export const loadGames = () => async (patch) => {
+export const loadGames = () => async (dispatch) => {
 	const popularData = await axios.get(popularGamesUrl());
-	patch({
+	dispatch({
 		type: "FETCH_GAMES",
 		payload: {
 			popular: popularData.data.results,
